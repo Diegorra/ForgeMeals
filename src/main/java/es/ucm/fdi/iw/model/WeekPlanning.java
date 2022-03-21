d3f008c13907fba7cb2d2c7e7606b23d8f5efc8a
oriavel <aveledoriana@gmail.com>
2022-03-21 21:42:16 +0100

    inicio paso 7

--- src/main/java/es/ucm/fdi/iw/model/WeekPlanning.java
+++ src/main/java/es/ucm/fdi/iw/model/WeekPlanning.java
@@ -1,4 +1,4 @@
-/*package es.ucm.fdi.iw.model;
+package es.ucm.fdi.iw.model;
 import lombok.Data;
 import javax.persistence.*;
 import java.util.*;
@@ -14,32 +14,31 @@ public class WeekPlanning{
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen")
     @SequenceGenerator(name = "gen", sequenceName = "gen")
     private long id;
+    
+    @OneToOne
     private User user;
    
     @Data
     private class Meal{
-        private OrderRecipe recipe;
-        private WeekDay weekday;
-        private Time time; 
+        private OrderRecipe recipe = new OrderRecipe(new Recipe("comidita ñam", " ", null), null);;
+        private WeekDay weekday = WeekDay.L;
+        private Time time  = Time.D;  
 
-        public Meal(){
-            weekday = WeekDay.L;
-            time = Time.D;
-            recipe = new OrderRecipe(new Recipe("comidita ñam", " ", null), null);
-        }
     }
-    // TODO se obtiene del controller creo
+    // se obtiene del controller creo
+    
+    @OneToOne
     private List<Meal> meals = new ArrayList<>();
 
     // las celdas se identifican por día y comida del día. Ej: celda 2,3 = merienda del miércoles 
     public String getMealByCell(int day, int time){
         for(Meal meal: meals){
             if(meal.getWeekday().ordinal() == day && meal.getTime().ordinal() == time){
-                return meal.getRecipe().getRecipe().getName(); // order -> recipe -> string
+                return meal.getRecipe().getRecipe().getName(); // Meal ->  OrderRecipe -> Recipe.string
             }
         }
 
         return "none";
     }
    
-}*/
+}
