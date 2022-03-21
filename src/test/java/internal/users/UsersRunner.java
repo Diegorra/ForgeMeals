2dea1a01200fd0ca770e9c630b58801d4e01fd96
Diego <56647811+Diegorra@users.noreply.github.com>
2022-03-21 13:53:06 +0100

    Actualizando tests

--- src/test/java/internal/users/UsersRunner.java
+++ src/test/java/internal/users/UsersRunner.java
@@ -1,12 +1,11 @@
-package examples.users;
+package internal.users;
 
 import com.intuit.karate.junit5.Karate;
 
 class UsersRunner {
-    
+
     @Karate.Test
     Karate testUsers() {
         return Karate.run("users").relativeTo(getClass());
-    }    
-
+    }
 }
